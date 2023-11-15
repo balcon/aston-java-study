134f28613e4d3bfdc04ee9788aeed2ad07cd6489
Konstantin Balykov <balcon@inbox.ru>
2023-11-16 01:57:12 +0300

    Implement reading db properties from yaml

--- src/main/java/com/github/balcon/venue/utils/ConnectionManager.java
+++ src/main/java/com/github/balcon/venue/utils/ConnectionManager.java
@@ -1,14 +1,34 @@
 package com.github.balcon.venue.utils;
 
 import lombok.experimental.UtilityClass;
+import org.yaml.snakeyaml.Yaml;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 @UtilityClass
 public class ConnectionManager {
+    private Map<String, String> dbProperties;
+
     static {
+        loadDriver();
+        loadProperties();
+    }
+
+    public Connection connection() {
+        try {
+            String url = dbProperties.get("url");
+            String username = dbProperties.get("username");
+            String password = dbProperties.get("password");
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadDriver() {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -16,14 +36,8 @@ public class ConnectionManager {
         }
     }
 
-    public Connection connection() {
-        try {
-            String url = "jdbc:postgresql://localhost:5433/postgres";
-            String username = "postgres";
-            String password = "postgres";
-            return DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    private void loadProperties() {
+        Yaml yaml = new Yaml();
+        dbProperties = yaml.load(ConnectionManager.class.getClassLoader().getResourceAsStream("db.yaml"));
     }
 }
