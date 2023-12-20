73c7d763d8efbe452dba953422965e2346c0553f
Konstantin Balykov <balcon@inbox.ru>
2023-12-20 12:36:33 +0300

    Cleanup tests

--- src/test/java/com/github/balcon/venue/TestData.java
+++ src/test/java/com/github/balcon/venue/TestData.java
@@ -1,6 +1,5 @@
 package com.github.balcon.venue;
 
-import com.github.balcon.venue.dto.BandDto;
 import com.github.balcon.venue.dto.BandWriteDto;
 import com.github.balcon.venue.dto.EventWriteDto;
 import com.github.balcon.venue.dto.MusicianWriteDto;
@@ -16,27 +15,26 @@ public final class TestData {
     public static final int MUSICIAN_ID = 120;
     public static final int EVENT_ID = 110;
 
-    public static final BandDto BAND = BandDto.builder()
-            .id(100)
-            .name("Band Number One").build();
-
     public static final Band BAND_DUMMY = Band.builder().build();
     public static final BandWriteDto BAND_WRITE_DTO_DUMMY = BandWriteDto.builder().build();
-    public static final BandWriteDto BAND_WRITE_DTO = BandWriteDto.builder()
-            .name("Name").build();
+    public static final BandWriteDto BAND_WRITE_DTO =
+            BandWriteDto.builder()
+                    .name("Name").build();
 
     public final static Event EVENT_DUMMY = Event.builder().build();
     public final static EventWriteDto EVENT_WRITE_DTO_DUMMY = EventWriteDto.builder().build();
-    public static final EventWriteDto EVENT_WRITE_DTO = EventWriteDto.builder()
-            .name("Name")
-            .dateTime(LocalDateTime.now()).build();
+    public static final EventWriteDto EVENT_WRITE_DTO =
+            EventWriteDto.builder()
+                    .name("Name")
+                    .dateTime(LocalDateTime.now()).build();
 
     public final static Musician MUSICIAN_DUMMY = Musician.builder().build();
     public final static MusicianWriteDto MUSICIAN_WRITE_DTO_DUMMY = MusicianWriteDto.builder().build();
-    public static final MusicianWriteDto MUSICIAN_WRITE_DTO = MusicianWriteDto.builder()
-            .name("Name")
-            .role("Role")
-            .bandId(BAND_ID).build();
+    public static final MusicianWriteDto MUSICIAN_WRITE_DTO =
+            MusicianWriteDto.builder()
+                    .name("Name")
+                    .role("Role")
+                    .bandId(BAND_ID).build();
 
     private TestData() {
     }
